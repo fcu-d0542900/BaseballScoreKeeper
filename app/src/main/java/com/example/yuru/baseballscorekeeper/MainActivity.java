a152f727ef71b93aa4f502d88b46f41996652f49
YURU <rubystage@gmail.com>
2018-06-07 23:44:21 +0800

    no message

--- app/src/main/java/com/example/yuru/baseballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.yuru.baseballscorekeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void fn_player(View view) {
+        Intent intent = new Intent(MainActivity.this,PlayerActivity.class);
+        intent.putExtra("n",1);
+        startActivityForResult(intent,111);
+
+    }
+    public void fn_record(View view) {
+        Intent intent = new Intent(MainActivity.this,RecordActivity.class);
+        intent.putExtra("n",1);
+        startActivityForResult(intent,111);
     }
 }
