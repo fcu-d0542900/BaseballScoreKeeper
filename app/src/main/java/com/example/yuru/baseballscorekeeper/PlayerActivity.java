735592a5c604be5f5190f2d4385ea1f17d363ce4
YURU <rubystage@gmail.com>
2018-06-08 19:41:39 +0800

    no message

--- app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerActivity.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +37,33 @@ public class PlayerActivity extends AppCompatActivity {
         rLayoutManager = new LinearLayoutManager(this);
         item_list.setLayoutManager(rLayoutManager);
 
+/*        // 建立RecyclerView元件的資料來源物件
+        itemAdapter = new PlayerAdapter(item_player, this) {
+            @Override
+            public void onBindViewHolder(final ViewHolder holder, final int Position) {
+                super.onBindViewHolder(holder, Position);
+
+                // 建立與註冊項目點擊監聽物件
+                holder.rootView.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // 讀取選擇位置的項目物件
+                                Player player = item_player.get(Position);
+                            }
+                        }
+                );
+
+            }
+        };*/
+
+        // 設定RecyclerView使用的資料來源物件
+        item_list.setAdapter(itemAdapter);
+
+        // 建立與設定項目操作物件
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(itemAdapter);
+        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+        touchHelper.attachToRecyclerView(item_list);
     }
 
 
--- app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerAdapter.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -33,8 +32,7 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
     public ViewHolder onCreateViewHolder(
             ViewGroup parent, int viewType) {
         // 建立項目使用的畫面配置元件
-        View v = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.cardview_item01, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item01, parent, false);
         // 建立與回傳包裝好的畫面配置元件
         ViewHolder viewHolder = new ViewHolder(v);
         return viewHolder;
@@ -66,29 +64,29 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int Position) {
         // 讀取目前位置的項目物件
-        final Player player = item_player.get(position);
+        final Player player = item_player.get(Position);
 
         // 設定編號、名稱、說明與是否選擇
-        holder.id_text.setText(Long.toString(player.getId()));
+        holder.id_text.setText(player.getId());
         holder.name_text.setText(player.getName());
         holder.position_text.setText(player.getPosition());
 
         // 設定動畫效果
-        setAnimation(holder.rootView, position);
+        setAnimation(holder.rootView, Position);
     }
 
     // 設定動畫效果
-    private void setAnimation(View view, int position) {
+    private void setAnimation(View view, int Position) {
         // 如果是最後一個項目
-        if (position > lastPosition) {
+        if (Position > lastPosition) {
             // 建立 Animation 動畫物件, 指定效果為由左方滑入
             Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
             // 啟動動畫效果
             view.startAnimation(animation);
             // 儲存最後一個項目的位置
-            lastPosition = position;
+            lastPosition = Position;
         }
     }
 
@@ -98,8 +96,8 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
     }
 
     // 新增一個項目
-    public void add(Player fruit) {
-        item_player.add(fruit);
+    public void add(Player player) {
+        item_player.add(player);
         // 通知資料項目已經新增
         notifyItemInserted(item_player.size());
     }
