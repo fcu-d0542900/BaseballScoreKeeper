dc60bbd2c270153c4cd8c63d5132d12b53fa4cc8
YURU <rubystage@gmail.com>
2018-06-09 17:25:05 +0800

    no message

--- app/src/main/java/com/example/yuru/baseballscorekeeper/RecordActivity.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/RecordActivity.java
@@ -1,8 +1,11 @@
 package com.example.yuru.baseballscorekeeper;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -14,6 +17,9 @@ public class RecordActivity extends AppCompatActivity {
 
 
     public void clickAddRecord(View view) {
-
+        final View item = LayoutInflater.from(RecordActivity.this).inflate(R.layout.dialog_new_record, null);
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setView(item);
+        adb.show();
     }
 }
