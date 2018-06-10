dc26ece38ada4e64febb7faa2f378959ed2ed7b5
YURU <rubystage@gmail.com>
2018-06-11 03:35:34 +0800

    no message

--- app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerActivity.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/PlayerActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
