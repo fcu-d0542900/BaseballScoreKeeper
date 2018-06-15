c2df8c018d3eaeab0e5e00363af386b404182255
ahkui <ahkui@outlook.com>
2018-06-16 05:47:18 +0800

    update

--- app/src/main/java/com/baseball/RecordItem.java
+++ app/src/main/java/com/baseball/RecordItem.java
@@ -9,9 +9,23 @@ public class RecordItem implements Serializable {
     private int round;
     private boolean score=false;
 
+    private RecordItemCenter global;
+    private RecordItemFirstBase base;
+    private RecordItemOtherBase base1,base2,base3;
+
+    public void updateUI(RecordItemCenter global){
+
+    }
+    public void updatebaseUI(Object base,int base_int){
+        if (base_int == 0){
+            RecordItemFirstBase base0 = (RecordItemFirstBase) base;
+            base0.setShowOneViewVisibility(false);
+        }
+    }
     RecordItem(Player player,int round){
         this.attPlayer = player;
-        this.round = round;        DatabaseService.getInstance().write();
+        this.round = round;
+        DatabaseService.getInstance().write();
 
     }
 
