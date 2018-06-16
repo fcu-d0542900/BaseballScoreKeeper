a4c70a5dc0e156c45d66aa37080aa1fa13a1e81d
YURU <rubystage@gmail.com>
2018-06-16 21:32:18 +0800

    高飛犧牲打選單

--- app/src/main/java/com/example/yuru/baseballscorekeeper/FirstBaseDialog.java
+++ app/src/main/java/com/example/yuru/baseballscorekeeper/FirstBaseDialog.java
@@ -21,9 +21,12 @@ import java.util.List;
 
 public class FirstBaseDialog {
 
+    private RecordItemFirstBase recordItemFirstBase;
+
     private NewRecordActivity newRecordActivity;
     private RadioGroup radioGroup_sacrificeFly_type,radioGroup_sacrificeFly_direction;
     private RadioButton radioButton_sacrificeFly_type_high,radioButton_sacrificeFly_type_line;
+    private RadioButton radioButton_sacrificeFly_direction_7,radioButton_sacrificeFly_direction_8,radioButton_sacrificeFly_direction_9;
 
 /*
     private String[] nums = {"1","2","3","4","5","6","7","8","9"};
@@ -37,6 +40,7 @@ public class FirstBaseDialog {
     }
 
     public void setFirstBaseDialog() {
+        recordItemFirstBase = new RecordItemFirstBase();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(newRecordActivity);
         builder.setItems(new String[]{"擊出球","未擊出球"}, new DialogInterface.OnClickListener(){
@@ -57,20 +61,43 @@ public class FirstBaseDialog {
                                         radioGroup_sacrificeFly_type = view_scrifice_fly_dialog.findViewById(R.id.radioGroup_sacrificeFly_type);
                                         radioButton_sacrificeFly_type_high = view_scrifice_fly_dialog.findViewById(R.id.radioButton_sacrificeFly_high);
                                         radioButton_sacrificeFly_type_line = view_scrifice_fly_dialog.findViewById(R.id.radioButton_sacrificeFly_line);
+                                        radioGroup_sacrificeFly_direction = view_scrifice_fly_dialog.findViewById(R.id.radioGroup_sacrificeFly_direction);
+                                        radioButton_sacrificeFly_direction_7 = view_scrifice_fly_dialog.findViewById(R.id.radioButton_sacrificeFly_direction_7);
+                                        radioButton_sacrificeFly_direction_8 = view_scrifice_fly_dialog.findViewById(R.id.radioButton_sacrificeFly_direction_8);
+                                        radioButton_sacrificeFly_direction_9= view_scrifice_fly_dialog.findViewById(R.id.radioButton_sacrificeFly_direction_9);
+
                                         view_scrifice_fly_dialog.setPadding(10,10,10,10);
                                         scrifice_fly_builder.setView(view_scrifice_fly_dialog);
                                         scrifice_fly_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialogInterface, int i) {
                                                 int select_sacrificeFly_typeID = radioGroup_sacrificeFly_type.getCheckedRadioButtonId();
+                                                int select_sacrificeFly_directionID = radioGroup_sacrificeFly_direction.getCheckedRadioButtonId();
                                                 String select_sacrificeFly_type_str = null;
+                                                String select_sacrificeFly_direction_str = null;
                                                 if(radioButton_sacrificeFly_type_high.getId() == select_sacrificeFly_typeID) {
                                                     select_sacrificeFly_type_str = "高飛球";
                                                 }
                                                 else if(radioButton_sacrificeFly_type_line.getId() == select_sacrificeFly_typeID) {
                                                     select_sacrificeFly_type_str = "平飛球";
                                                 }
-                                                Toast.makeText(newRecordActivity.getApplicationContext(), "OK"+select_sacrificeFly_type_str, Toast.LENGTH_SHORT).show();
+                                                else {
+                                                    select_sacrificeFly_type_str = "未選擇";
+                                                }
+
+                                                if(radioButton_sacrificeFly_direction_7.getId() == select_sacrificeFly_directionID) {
+                                                    select_sacrificeFly_direction_str = "左外野";
+                                                }
+                                                else if(radioButton_sacrificeFly_direction_8.getId() == select_sacrificeFly_directionID) {
+                                                    select_sacrificeFly_direction_str = "中外野";
+                                                }
+                                                else if(radioButton_sacrificeFly_direction_9.getId() == select_sacrificeFly_directionID) {
+                                                    select_sacrificeFly_direction_str = "右外野";
+                                                }
+                                                else {
+                                                    select_sacrificeFly_direction_str = "未選擇";
+                                                }
+                                                Toast.makeText(newRecordActivity.getApplicationContext(), "OK "+select_sacrificeFly_type_str + "," + select_sacrificeFly_direction_str, Toast.LENGTH_SHORT).show();
                                             }
                                         });
                                         scrifice_fly_builder.show();
@@ -99,9 +126,10 @@ public class FirstBaseDialog {
                             public void onClick(View v) {
                                 Toast.makeText(newRecordActivity, "保送", Toast.LENGTH_SHORT).show();
                                 unhit_dialog.dismiss();
-                                RecordItemFirstBase recordItemFirstBase = new RecordItemFirstBase();
+
                                 recordItemFirstBase.setShowZeroViewVisibility(true);
-                                //TODO: ahkui 存入資料庫， 顯示圖片 B
+                                //TODO: ahkui 存入資料庫， 顯示圖片 B   (R.drawable.bad_ball)
+
                             }
                         });
                         view_unhit_dialog.findViewById(R.id.click_db).setOnClickListener(new View.OnClickListener() {
@@ -111,7 +139,8 @@ public class FirstBaseDialog {
                                 unhit_dialog.dismiss();
                                 RecordItemFirstBase recordItemFirstBase = new RecordItemFirstBase();
                                 recordItemFirstBase.setShowZeroViewVisibility(true);
-                                //TODO:ahkui 存入資料庫， 顯示圖片 D
+                                //TODO:ahkui 存入資料庫， 顯示圖片 D  (R.drawable.hit_by_pitch)
+
                             }
                         });
                         view_unhit_dialog.findViewById(R.id.click_k).setOnClickListener(new View.OnClickListener() {
@@ -121,7 +150,7 @@ public class FirstBaseDialog {
                                 unhit_dialog.dismiss();
                                 RecordItemFirstBase recordItemFirstBase = new RecordItemFirstBase();
                                 recordItemFirstBase.setShowZeroViewVisibility(true);
-                                //TODO:ahkui 存入資料庫， 顯示圖片 K
+                                //TODO:ahkui 存入資料庫， 顯示圖片 K  (R.drawable.killed)
                             }
                         });
                         view_unhit_dialog.findViewById(R.id.click_nok).setOnClickListener(new View.OnClickListener() {
@@ -131,7 +160,8 @@ public class FirstBaseDialog {
                                 unhit_dialog.dismiss();
                                 RecordItemFirstBase recordItemFirstBase = new RecordItemFirstBase();
                                 recordItemFirstBase.setShowZeroViewVisibility(true);
-                                //TODO:ahkui 存入資料庫， 顯示圖片 倒K
+                                //TODO:ahkui 存入資料庫， 顯示圖片 倒K  (R.drawable.no_killed)
+
                             }
                         });
                         unhit_dialog.show();
