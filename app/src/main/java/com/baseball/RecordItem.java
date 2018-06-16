e85c39d90e578d56600f2132a839dd0e4e876c46
ahkui <ahkui@outlook.com>
2018-06-16 22:25:15 +0800

    update code bdkkr

--- app/src/main/java/com/baseball/RecordItem.java
+++ app/src/main/java/com/baseball/RecordItem.java
@@ -4,15 +4,25 @@ import java.io.Serializable;
 import java.util.List;
 
 public class RecordItem implements Serializable {
+    // metric infomation
+    private int round;
+    private int row;
+    private int column;
+
+    // user infomation
     private Player attPlayer;
     private List<Player> defPlayer;
-    private int round;
     private boolean score=false;
+
+    // ui information
     private boolean isShow_BASE_B_D_K_KR = false;
     private BASE_B_D_K_KR base_step; // TODO change this variable name YURU ZENGLA, i am no idea...
 
     public void updateUI(RecordItemCenter center,RecordItemFirstBase base,RecordItemOtherBase base1,RecordItemOtherBase base2,RecordItemOtherBase base3){
-this .updateFirstBaseUI(base);
+        this.updateFirstBaseUI(base);
+        this.updateOtherBaseUI(base1,1);
+        this.updateOtherBaseUI(base2,2);
+        this.updateOtherBaseUI(base3,3);
     }
 
     public void setBASE_B_D_K_KR(boolean state,BASE_B_D_K_KR value){
@@ -52,9 +62,11 @@ this .updateFirstBaseUI(base);
         }
     }
 
-    RecordItem(Player player,int round){
+    RecordItem(Player player,int round,int row,int column){
         this.attPlayer = player;
         this.round = round;
+        this.row = row;
+        this.column = column;
         DatabaseService.getInstance().write();
 
     }
